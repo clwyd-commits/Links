0a984f3647cc29dac423c4f6cd88436d80af2e16
Clwyd <clwyd@pathtracerpictures.com>
2026-05-20 22:03:01 +0400

    Initial Commit

---
+++ Links/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Links
+//
+//  Created by Clwyd on 20/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Links/LinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinksApp.swift
+//  Links
+//
+//  Created by Clwyd on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
